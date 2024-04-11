4d9c1d517fc709fc0a961111550cc4ece0378eb3
Elviscryptic1981 <Elviskimani99@gmail.com>
2024-04-11 21:21:22 +0300

    Create Calc

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("btn").onclick = function() {
+    const input = document.getElementById("input").value;
+    let sqrt = Math.sqrt(input);
+    document.getElementById("answer").innerText = "(" + sqrt + ")²";
+};
